940d39bba529eca26ed9846711c893a21a741c7a
Etudiant <etudiant@Halle6-modele-7.local>
2022-01-14 09:22:27 +0100

    init xcode

---
+++ 21Parks/21Parks/_1ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1ParksApp.swift
+//  21Parks
+//
+//  Created by Etudiant on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct _1ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
