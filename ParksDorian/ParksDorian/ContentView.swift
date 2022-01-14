8a31687e413088d0c53bd6d88789e1cc1f5750ec
Etudiant <etudiant@Halle6-modele-7.local>
2022-01-14 09:54:29 +0100

    deuxieme project

---
+++ ParksDorian/ParksDorian/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  ParksDorian
+//
+//  Created by Etudiant on 14/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Park")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            HStack{
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                    Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ParksDorian/ParksDorian/ParksDorianApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksDorianApp.swift
+//  ParksDorian
+//
+//  Created by Etudiant on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ParksDorianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
