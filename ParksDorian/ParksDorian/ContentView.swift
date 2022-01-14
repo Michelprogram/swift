50fbfe82ef99743b993ebdf6fde88bffe19af12e
Etudiant <etudiant@Halle6-modele-7.local>
2022-01-14 10:27:50 +0100

    ViewCustomDansLaViewPrincipale

--- ParksDorian/ParksDorian/ContentView.swift
+++ ParksDorian/ParksDorian/ContentView.swift
@@ -9,18 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Park")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            HStack{
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                    Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack{
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Park")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                HStack{
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                        Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
         }
-        .padding()
     }
 }
 
