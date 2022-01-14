b57bb494844d850e6e7791688e9fc2dfe72b95c2
Etudiant <etudiant@Halle6-modele-7.local>
2022-01-14 10:09:02 +0100

    Image

---
+++ ParksDorian/ParksDorian/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  ParksDorian
+//
+//  Created by Etudiant on 14/01/2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
