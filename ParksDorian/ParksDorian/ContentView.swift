56c7e547261ee96468c2f31f7daffd5f18bc505f
Etudiant <etudiant@Halle6-modele-7.local>
2022-01-14 10:51:34 +0100

    AdjustPosition

--- ParksDorian/ParksDorian/ContentView.swift
+++ ParksDorian/ParksDorian/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack{
             MapView()
+                .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
             CircleImage()
                 .offset(y: -130)
@@ -20,13 +21,19 @@ struct ContentView: View {
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 HStack{
                     Text("Joshua Tree National Park")
-                        .font(.subheadline)
                         Spacer()
                     Text("California")
-                        .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                Text("About")
+                    .font(.title2)
+                Text("Description")
             }
             .padding()
+            Spacer()
         }
     }
 }
